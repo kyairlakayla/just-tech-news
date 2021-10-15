9f2faf5a127bdc2403ec270c1933b4a3d8fa4624
Kayla <kyairlakayla.davis@gmail.com>
2021-10-15 14:45:58 -0400

    show nav features if a user is logged in

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,8 +29,12 @@ router.get('/', (req, res) => {
         ]
     }).then(dbPostData => {
         const posts = dbPostData.map(post => post.get({ plain: true})); // loops and maps each object into a serialized version -> saving results in a new posts array
-        res.render('homepage', { posts });
-    }).catch(err => {
+        res.render('homepage', { 
+            posts,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
         console.log(err);
         res.status(500).json(err);
     });

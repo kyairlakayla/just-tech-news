7c4caa9d27234002e13ebf3c51b5d86b5ced8380
Kayla <kyairlakayla.davis@gmail.com>
2021-10-15 14:33:51 -0400

    allow comment/upvote features only to logged in users

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { response } = require('express');
 const { Comment } = require('../../models');
 
 router.get('/', (req, res) => {
@@ -11,16 +12,18 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    })
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            user_id: req.session.user_id,
+            post_id: req.body.post_id
+        })
+        .then(dbCommentData => res.json(dbCommentData)
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        }))
+    }
 });
 
 router.delete('/:id', (req, res) => {
